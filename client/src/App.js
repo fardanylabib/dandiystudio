f0a50b16cef1d9f7dcfcc97ebb2e9cb8b323cc37
Biblab <labibfardany@gmail.com>
2019-12-03 08:14:06 +0700

    init environment: - create-react-app - bulma - node-sass

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+	  <div>
+		<section className="section">
+		  <div className="container">
+			<h1 className="title">
+			  Hello World
+			</h1>
+			<p className="subtitle">
+			  My first website with <strong>Bulma</strong>!
+			</p>
+			<button className="button is-primary">Tombol</button>
+		  </div>
+		</section>
+	  </div>
+	);
+}
+
+export default App;
