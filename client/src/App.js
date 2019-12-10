93ef34fe8af7ddb4ce483830640aa96659790c83
Biblab <fardanylabib@gmail.com>
2019-12-10 12:00:06 +0700

    add pictures

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,41 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
+const data = ['a','b','c','d','e','f','g']
+
+const Card = () => (
+	<div className = "column is-half">
+	<div className="card">
+		<div className="card-image">
+			<figure className="image is-4by3">
+			<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+			</figure>
+		</div>
+		<div className="card-content">
+			<div className="media">
+				<div className="media-left">
+					<figure className="image is-48x48">
+						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+					</figure>
+				</div>
+				<div className="media-content">
+					<p className="title is-4">John Smith</p>
+					<p className="subtitle is-6">@johnsmith</p>
+				</div>
+			</div>
+
+			<div className="content">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+			<a href="#">#css</a> <a href="#">#responsive</a>
+			<br/>
+			<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+			</div>
+		</div>
+	</div>
+	</div>
+);
+
 function App() {
   return (
 	<div>
@@ -38,13 +73,21 @@ function App() {
 							Go Digital with Us!
 						</h1>
 						<h2 className="subtitle">
-							DandiyStudio melakukan 'modernisasi' pada bisnis Anda
+							Buat bisnis Anda menjadi lebih modern dengan DandiyStudio
 						</h2>
 						</div>
 					</div>
 				</section>
 			</div>
 			<div className="app-content">
+				<div className = "row columns">
+				{
+					data.map((item) =>(
+						<Card key={item}/>
+					))
+				}
+				</div>
+
 				<p>
 					Lorem ipsum
 				</p>
