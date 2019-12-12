cf26d19f68ce4032ee6f5cedf931518fe33e9eb0
Biblab <fardanylabib@gmail.com>
2019-12-12 14:22:11 +0700

    set card title and price

--- client/src/App.js
+++ client/src/App.js
@@ -2,38 +2,62 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-const data = ['a','b','c','d','e','f','g']
+const data = [
+				{
+					row : 0,
+					col: [{title: 'Desain Logo', price: 25, discount: 0},
+							{title: 'Desain Spanduk/ Banner', price: 30, discount: 0}]
+				},
+				{
+					row : 1,
+					col: [{title: 'Desain Email Marketing', price: 50, discount: 0},
+							{title: 'Website Company Profile', price: 200, discount: 0}]
+				},
+				{
+					row: 2,
+					col: [{title: 'Toko Online Pribadi', price: 500, discount: 0}]
+				}
+			];
 
-const Card = () => (
-	<div className = "column is-half">
-	<div className="card">
-		<div className="card-image">
-			<figure className="image is-4by3">
-			<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-			</figure>
-		</div>
-		<div className="card-content">
-			<div className="media">
-				<div className="media-left">
-					<figure className="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-					</figure>
+const Card = ({rowData}) => (
+	<div className = "row columns">
+		{
+			rowData.col.map((colData) => (
+				<div className = "column is-half">
+					<div className="card">
+						<div className="card-image">
+							<figure className="image is-4by3">
+							<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+							</figure>
+						</div>
+						<div className="content">
+							<table>
+								<tr>
+									<td  className = "service-title">
+										<h5>
+										{
+											colData.title
+										}
+										</h5>
+									</td >
+									<td className = "service-from">
+										<p>Start from</p>
+									</td>
+									<td className = "service-price">
+										<h4>
+											Rp&nbsp;
+											{
+												colData.price - colData.discount
+											}k
+										</h4>
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
 				</div>
-				<div className="media-content">
-					<p className="title is-4">John Smith</p>
-					<p className="subtitle is-6">@johnsmith</p>
-				</div>
-			</div>
-
-			<div className="content">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-			<a href="#">#css</a> <a href="#">#responsive</a>
-			<br/>
-			<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-			</div>
-		</div>
-	</div>
+			))
+		}
 	</div>
 );
 
@@ -73,117 +97,18 @@ function App() {
 							Go Digital with Us!
 						</h1>
 						<h2 className="subtitle">
-							Buat bisnis Anda menjadi lebih modern dengan DandiyStudio
+							Bikin bisnis Anda menjadi lebih modern dengan DandiyStudio
 						</h2>
 						</div>
 					</div>
 				</section>
 			</div>
 			<div className="app-content">
-				<div className = "row columns">
 				{
 					data.map((item) =>(
-						<Card key={item}/>
+						<Card key={item.row} rowData={item}/>
 					))
 				}
-				</div>
-
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
-				<p>
-					Lorem ipsum
-				</p>
 			</div>
 		</div>
 		
